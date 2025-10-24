685dafe94b54f20e213f80f09d5a3b5a0bbe2d6b
LCH-1228 <chanho1228@gmail.com>
2025-10-25 00:18:35 +0900

    design: UIFont Set 추가
    - Resources에 PretendardVariable.ttf 파일 추가
    - Font 사용을 위한 Tuist Project.Swift infoPlist에 UIAppFonts Key-Value 추가
    - Font 로딩 실패시 동일한 Weight로 시각적 유사함을 주기위한 FontUtility 구현
    - UIFont extension을 통한 Font Set 추가
    
    Ref: #5

---
+++ BillKeeper/Sources/Shared/Utilities/FontUtility.swift
@@ -0,0 +1,31 @@
+//
+//  FontUtility.swift
+//  BillKeeper
+//
+//  Created by LCH on 10/24/25.
+//
+
+import UIKit
+
+enum FontUtility {
+  enum Weight: String {
+    case semiBold = "PretendardVariable-SemiBold"
+    case medium = "PretendardVariable-Medium"
+    case regular = "PretendardVariable-Regular"
+  }
+  
+  static func resolveFont(weight: Weight, size: CGFloat) -> UIFont {
+    if let font = UIFont(name: weight.rawValue, size: size) {
+      return font
+    } else {
+      switch weight {
+        case .semiBold:
+          return .systemFont(ofSize: size, weight: .semibold)
+        case .medium:
+          return .systemFont(ofSize: size, weight: .medium)
+        case .regular:
+          return .systemFont(ofSize: size, weight: .regular)
+      }
+    }
+  }
+}
