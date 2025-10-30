60c92be85791b821af0f8de78f745d2dcdcbdbb6
LCH-1228 <chanho1228@gmail.com>
2025-10-30 22:52:03 +0900

    feat(domain): RepositoryError 추가
    - Domain/Repositories에 RepositoryError enum 추가
    - Repository 구현체에서 공통으로 사용하는 기본 에러 정의
    
    Ref: #22

---
+++ BillKeeper/Sources/Domain/Repositories/RepositoryError.swift
@@ -0,0 +1,13 @@
+//
+//  RepositoryError.swift
+//  BillKeeper
+//
+//  Created by LCH on 10/28/25.
+//
+
+import Foundation
+
+enum RepositoryError: Error, Equatable {
+  case notFound
+  case relatedEntityNotFound(String)
+}
