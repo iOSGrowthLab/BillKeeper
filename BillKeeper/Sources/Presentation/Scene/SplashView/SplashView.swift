7c2d496d97524791f4424a0ccdcf50a4d99d66b4
LCH-1228 <chanho1228@gmail.com>
2025-12-11 21:10:37 +0900

    feat: SplashView UILayout 초기 구현 및 Logo 이미지 Asset 추가
    - 로직을 제외한 UILayout 초기 구현
    
    Ref: #29

---
+++ BillKeeper/Sources/Presentation/Scene/SplashView/SplashView.swift
@@ -0,0 +1,40 @@
+//
+//  SplashView.swift
+//  BillKeeper
+//
+//  Created by LCH on 11/17/25.
+//
+
+import ReactorKit
+import RxCocoa
+import RxSwift
+import UIKit
+
+class SplashView: UIViewController, ViewConfigurable {
+  private let imageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.image = UIImage(resource: .logo)
+    imageView.contentMode = .scaleAspectFit
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    return imageView
+  }()
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupUI()
+  }
+
+  func setupView() {
+    view.addSubview(imageView)
+    view.backgroundColor = .Brand.primary
+  }
+
+  func setupLayout() {
+    NSLayoutConstraint.activate([
+      imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+      imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor),
+      imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 24),
+      imageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -24)
+    ])
+  }
+}
